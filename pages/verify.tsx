b6af84281bc79c568640db3c6ef2f09d45533148
miyawaki100 <miyawaki100eng@gmail.com>
2025-11-15 20:12:19 +0900

    fix: disable firebase auth usage in verify page

---
+++ pages/verify.tsx
@@ -0,0 +1,10 @@
+"use client";
+
+// Firebase Auth を使わないので、このページはダミー表示に変更します。
+export default function Verify() {
+  return (
+    <div style={{ padding: 40 }}>
+      メール認証ページは現在使用していません。
+    </div>
+  );
+}
